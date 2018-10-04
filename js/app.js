0ae67645f4427ab346d41ffcaab8ef348f518214
Kevinrosales <kevinrosalesolvera@gmail.com>
2018-10-04 14:53:36 -0700

    finished the project

--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
 'use strict'
 
- var answer1, answer2, answer3, answer4, answer5, answer6, answer7;
-
-
+ var answer1, answer2, answer3, answer4, answer5, answer6, answer7,score;
+ score = 0
 
 alert('Hi there think you know me? well i\'ve prepared this quiz for you to test how good of a friend you are to me. Make sure to answer with yes & no or y & n. good luck!');
 
@@ -11,13 +10,15 @@ if (answer1 ==='yes' || answer1 ==='y') {
     alert('Dang sorry! I have actually never jumped out of a plane willingly... or unwillingly')
 }
 else if (answer1 ==='no' || answer1 ==='n') {
-    alert('Yup I have never tried it nor do I think I will ever try it.')
+    alert('Yup I have never tried it nor do I think I will ever try it.');
+    score = score +1;
 }
 console.log('answer one ' + answer1);
 
 answer2 = prompt('Do I love dolphins?').toLowerCase();
 if (answer2 ==='yes' || answer2 ==='y') {
-    alert('Nice. I actually love dolphins more than most people')
+    alert('Nice. I actually love dolphins more than most people');
+    score = score +1;
 }
 else if (answer2 ==='no' || answer2 ==='n'){
     alert('Nope you cant really get more wrong then that pal')
@@ -29,28 +30,32 @@ if (answer3 ==='yes' || answer3 ==='y'){
     alert('Sorry but No.... no it is not')
 }
 else if (answer3 ==='no' || answer3 ==='n'){
-    alert('Yup I was forced to play it in middle school and now I hate it :c')
+    alert('Yup I was forced to play it in middle school and now I hate it :c');
+    score = score +1;
+
 }
 console.log('answer three ' + answer3);
 
 answer4 = prompt('Am I afraid of needles?').toLowerCase();
 if (answer4 ==='yes' || answer4 ==='y'){
-    alert('Yup big time. My absolute biggest fear is needles I dont know why but thats the one thing I cant handle.')
+    alert('Yup big time. My absolute biggest fear is needles I dont know why but thats the one thing I cant handle.');
+    score = score +1;
+
 }
 else if (answer4 ==='no' || answer4 ==='n'){
     alert('If only it where true the last time I got a shot I started to hyperventilate then passed out.')
 }
 console.log('answer four ' + answer4);
 
-answer5 = prompt('Do I like teriaki?').toLowerCase();
+answer5 = prompt('Do I like teriyaki?').toLowerCase();
 if (answer5 ==='yes' || answer5 ==='y'){
     alert('another big NO. I got food poisoning the first time I ate teriaki and that kind of ruined it for me.')
 }
 else if (answer5 ==='no' || answer5 ==='n'){
-    alert('Yeah I cant eat teriaki without thinking about the time I got food poisoning from it.')
+    alert('Yeah I cant eat teriyaki without thinking about the time I got food poisoning from it.');
+    score = score +1;
 }
 console.log('answer five ' + answer5);
-
 
 // lab3 starts here
 
@@ -65,12 +70,11 @@ while(trys >= 1)
     if(parseInt(answer6)===rightAnswer){
        alert('Holy cow yeah thats right my favorite number is 19!');
        trys = 0;
-        
+       score = score +1;
     }
     else if(parseInt(answer6) > 20){
         alert('Hey! i said 1 through 20!');
        answer6 = prompt('out of 1 through 20 what is my favorite number');
-    
     }
 
     else if(parseInt(answer6) > rightAnswer)
@@ -98,9 +102,6 @@ while(trys >= 1)
 
 favNum();
 
-
-
-
 function favFood(){
     answer7 = prompt('Try to guess what my favorite foods are!');
     var foods = ['curry','tacos','pizza'];
@@ -114,20 +115,21 @@ function favFood(){
                     alert("Correct curry is one of my favorites");
                     guesses = guesses -1 ;
                     isCorrect = true;
+                    score = score +1;
                     break;
-
 
                 case 'tacos':
                     alert("Correct tacos is one of my favorites");
                     guesses = guesses -1 ;
                     isCorrect = true;
+                    score = score +1;
                     break;
-                    
 
                 case 'pizza':
                     alert("Correct pizza is one of my favorites");
                     guesses = guesses -1 ;
                     isCorrect = true;
+                    score = score +1;
                     break;
 
                 default:
@@ -143,7 +145,6 @@ function favFood(){
 
     }
     
-            }
-    
-
-favFood();
+}
+    favFood();
+alert('You got ' + score +' out of seven correct')
